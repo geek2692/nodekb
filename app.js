1d0dcec3817ad11c4673c1ada5bdd13d37e1308d
geek2692 <geek2692@gmail.com>
2017-10-17 19:39:26 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// Init App
+const app = express();
+
