bf2033f994b8138e2f8c506b7d1af2d55272c666
geek2692 <geek2692@gmail.com>
2017-10-17 20:15:56 +0900

    setting up article model

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const config = require('./config/database');
+
+mongoose.connect(config.database);
+let db = mongoose.connection;
 
 // Init App
 const app = express();
